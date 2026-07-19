8ca7006d7467c713c0400071f9257da6c872c9e1
Simran Bhimjyani <simranbhimjyani.859@gmail.com>
2026-07-19 23:15:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,133 @@
+// Definitions of resilience shared by women in Digital Humanities in India.
+// Each entry was published according to the contributor's chosen attribution.
+// To add a new (vetted) voice, append an object to this array.
+const VOICES = [
+  {
+    "quote": "Resilience for me in my DH practice is always making the best out of any given situation. It means having faith in the process and the belief that eventually everything will work out for itself only if I refuse to give up.",
+    "name": "Sudeshna",
+    "affiliation": "Independent researcher",
+    "link": "",
+    "anonymous": false,
+    "areas": "Archival Studies, Cultural Mapping"
+  },
+  {
+    "quote": "Maintaining my determination to keep my passion for search for information alive by trying to make time and put effort in reaching/finding places of archive such as digital space, internet, archival institutions, etc.",
+    "name": "Pompi Basumatary",
+    "affiliation": "",
+    "link": "",
+    "anonymous": false,
+    "areas": "Archival Studies, Cultural Mapping"
+  },
+  {
+    "quote": "Resilience to me means to keep going in the direction that your research hypothesis points out, even if the results don't show up in the way you expect them, no matter how much they disappoint you, you just keep going and one day all the failed results will make sense when you find out the right way to tackle that one big research question.",
+    "name": "Anonymous",
+    "affiliation": "",
+    "link": "",
+    "anonymous": true,
+    "areas": "Text Analysis"
+  },
+  {
+    "quote": "Carrying on despite all odds!",
+    "name": "Maya Dodd",
+    "affiliation": "",
+    "link": "https://mayadodd.in",
+    "anonymous": false,
+    "areas": "Archival Studies"
+  },
+  {
+    "quote": "To me, resilience in Digital Humanities means remaining adaptable, curious, and committed to learning in an ever-evolving technological landscape. Coming from a background in Persian Language, Literature, and Cultural Studies, I often encounter challenges such as limited digital resources for Persian and Indo-Persian materials, the need to acquire new technical skills, and the complexities of interdisciplinary collaboration. Resilience, therefore, involves embracing these challenges as opportunities for growth, continuously learning new methodologies, and finding creative ways to bridge traditional humanities scholarship with digital tools. It is the ability to persevere despite obstacles while remaining open to innovation, collaboration, and new forms of knowledge production.",
+    "name": "Zeenat Kaifee",
+    "affiliation": "Department of Persian, T.N.B. College, T.M. Bhagalpur University, Bihar",
+    "link": "",
+    "anonymous": false,
+    "areas": "Archival Studies"
+  },
+  {
+    "quote": "In my DH practice, resilience means continually adapting my methods to overcome complex data, changing technology, and institutional hurdles. It is about staying committed to long-term research despite these challenges. Ultimately, resilience helps me turn research roadblocks into creative opportunities to share meaningful and accessible knowledge.",
+    "name": "Vedalakshmi G",
+    "affiliation": "Associate Professor of History, GFGC Madhugiri, Tumkur University",
+    "link": "https://orcid.org/0000-0001-6122-7768",
+    "anonymous": false,
+    "areas": "Archival Studies, Cultural Mapping, Digital Pedagogy"
+  },
+  {
+    "quote": "To be able to withstand technological obsolescence, data loss and institutional constraints especially belonging from Northeasr region.",
+    "name": "Dr. Gitanjali Roy",
+    "affiliation": "Assistant Professor and Coordinator, FLA, ICFAI University",
+    "link": "",
+    "anonymous": false,
+    "areas": "Archival Studies, Text Analysis, Cultural Mapping, Digital Pedagogy"
+  },
+  {
+    "quote": "Firstly, I personally am not a big fan of the word 'resilience'. Why? Because it reminds me of personal (emotional and intellectual) labor that one is being rewarded by, if they fight through and against a broken system---labor that by the way is not absolute or necessary but the product of that very system. \n\nThat being said, in context of DH: For me, resilience in DH means to nourish the freedom, courage, and intellectual curiosity that it provides. \n\nMy research sits at the cusp of controversial disciplinary debates about DH, tech, social sciences and challenges dominant narratives. For me, resilience is staying true to myself and pursuing the unique lens that I bring in with my socially and technically informed lens. When not many people understand or agreed to my area of research (Critical AI/Algorithm/Data Studies) as something that DH must engage with, I explored and experimented with my instincts, training, and lots and lots of self-learning. \n\nDH is a trans-disciplinary bridge, which gives immense space to explore and experiment in both theory and practice. I am aware there are infrastructural issues but I believe in open source and self-learning. Much of IT and AI resources are now being shared by the Indian government and some big tech companies, plus ample resources on domain knowledge. We can do good work with a good cohort or community and internet where we can learn coding, or use open tools, or discuss or experiment, all those things. My observations on resilience in DH is less about infrastructure, more about people and practice.",
+    "name": "Vasundhra Dahiya",
+    "affiliation": "IIT Jodhpur",
+    "link": "https://orcid.org/0000-0002-8646-8450",
+    "anonymous": false,
+    "areas": "Digital Pedagogy, Critical AI, Algorithm, and Data Studies ; Algorithmic Accountability ; Data ethics and Responsible computing"
+  },
+  {
+    "quote": "Paving my way through the study that combines many spheres of study, many disciplines, and different platforms. There is a lack of clear methodologiew, instruments, codes etc. so resilience to me means a lot of trial and error, staring at the sky, picking up myself, marching on reaching out to fellow researchers. \n\nMy work sits on the intersection of technology, feminist studies, politics, artivism.",
+    "name": "Anonymous",
+    "affiliation": "Assistant Professor, Christ (Deemed to be University)",
+    "link": "",
+    "anonymous": true,
+    "areas": "Text Analysis, Cultural Mapping, Digital Pedagogy"
+  },
+  {
+    "quote": "It means the constant need to overcome difficulties in terms of acquiring technical skills as a humanities researcher and also in dealing/growing with failures. Waiting for funding and infrastructural improvements so that the basic requirements for DH research are met. Overcoming mental exhaustion of having to do the \"double work\" - digital and humanities - all because I want my research to be open-access and technically advanced.",
+    "name": "Anonymous",
+    "affiliation": "",
+    "link": "",
+    "anonymous": true,
+    "areas": "Archival Studies, Cultural Mapping, Spatial Archiving"
+  },
+  {
+    "quote": "DH in India was fairly new when I was first introduced to it, and it has come a long way since then. With little to no resources available and, importantly, with no framework to lean on, we have, hopefully, managed to create meaningful discourse in the field. And this was possible because of our resilience. So, DH definitely requires a set of skills, but resilience to keep learning and unlearning.",
+    "name": "Wati",
+    "affiliation": "",
+    "link": "",
+    "anonymous": false,
+    "areas": "Archival Studies"
+  },
+  {
+    "quote": "In my practice it means to adapt with technology in general and codes in particular.\nNever had I imagined that a text can be analysed through codes.",
+    "name": "Diptashree Banerjee",
+    "affiliation": "IIT (ISM) Dhanbad",
+    "link": "",
+    "anonymous": false,
+    "areas": "Data Visualization, Text Analysis"
+  },
+  {
+    "quote": "My experience as a woman in academia and DH has made me more patient and to believe in resilience as sometimes the criticism and not positive approach can be very difficult to face.",
+    "name": "Papia Sengupta",
+    "affiliation": "Jawaharlal Nehru University",
+    "link": "",
+    "anonymous": false,
+    "areas": "Archival Studies, Data Visualization, Text Analysis, Digital Pedagogy"
+  },
+  {
+    "quote": "Resilience in Digital Humanities, for me, is not simply about enduring difficulty. It is about remaining curious when certainty is absent, continuing to learn despite feeling like an outsider, and believing that experimentation has scholarly value even when others question it. But it took me some time to accept this. Before that, I went through self-doubt; it was a test of patience, too. It also made me think I should have stayed with my traditional humanities scholarship, as the labor and time involved in a DH study with quite overwhelming. However, besides the challenges, working and experimenting with the software and developing new methods of looking at culture was exciting too and the excitement kept me going. Resilience has meant transforming fear into inquiry, hesitation into learning, and skepticism into motivation. Every map, network, visualization, and dataset I created became more than a research output - they became evidence that I belonged in this space and could contribute to shaping it.",
+    "name": "Barkha Rani Lakra",
+    "affiliation": "IIT (ISM) Dhanbad",
+    "link": "",
+    "anonymous": false,
+    "areas": "Data Visualization, Cultural Mapping"
+  },
+  {
+    "quote": "In academic institutions, people are not very accepting of interdisciplinary studies, since there is still a concrete outcome expected from subjects like DH. The outcome here is often understood in reference to the pay package a student could receive post the degree. Unfortunately, DH practices are explored in institutions as a fancy idea that includes technology or AI and might fascinate students; it might help in NAAC inspections by showing updated courses adapted by the university. But the reality remains that DH practices exist either in offerings of Certificate courses or are practiced within a document of the research done by traditional departments where DH nomenclature is used in the concluding chapter of research. The resilience in this scenario is seen as an act where faculty, researchers, and scholars continue their DH practices through independent projects. The progress of this resilience can be witnessed in the fact that, gradually, the files of DH Certificate courses have reached the table of the Board of Studies. The deliberations continue with the hope that soon DH shall be given the reverence it deserves.",
+    "name": "Anonymous",
+    "affiliation": "",
+    "link": "",
+    "anonymous": true,
+    "areas": "Archival Studies, Text Analysis, Digital Pedagogy"
+  },
+  {
+    "quote": "Being able to see opportunities in mistakes or unexpected situations; thinking about creative alternatives on-the-go and improvisation to accommodate diverse opinions; focusing more on the process rather than the outcome; bouncing back after a fall and a small break; letting things be or taking a pause if that is in better interest of the practice instead of solving and looking for quickfixes; celebrating small goalposts, and even survival through crisis",
+    "name": "Ishita Shah",
+    "affiliation": "Curating for Culture",
+    "link": "https://www.curatingforculture.com",
+    "anonymous": false,
+    "areas": "Archival Studies, Cultural Mapping, Web Development, Curation and Community Engagement"
+  }
+];
